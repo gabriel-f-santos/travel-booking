d89d79a9673f87b539be5b78793f2d7abc018cfd
Gabriel <gabrielfgsantos95@gmail.com>
2024-07-28 16:48:08 -0300

    feat: add navbar

--- next-app/travel-app/src/components/navbar/NavBar.jsx
+++ next-app/travel-app/src/components/navbar/NavBar.jsx
@@ -1,6 +1,25 @@
+"use client";
 import React from 'react'
 
 export default function NavBar() {
+  const [showModal, setShowModal] = React.useState(false);
+  const [isScrolling, setIsScrolling] = React.useState(false);
+
+  const toggleModal = () => {
+    setShowModal(!showModal);
+  }
+  
+  React.useEffect(() => {
+    window.onscroll = () => {
+      setIsScrolling(window.pageYOffset === 0 ? false : true);
+    }
+
+    return () => {
+      window.onscroll = null;
+    }
+
+  }, []);
+
   return (
     <div>NavBar</div>
   )
