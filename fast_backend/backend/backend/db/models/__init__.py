a5b606e7541a48d94af6a5cc5b0a78eb6b5389bd
Gabriel <gabrielfgsantos95@gmail.com>
2024-07-28 16:51:44 -0300

    feat: add backend from https://github.com/s3rius/FastAPI-template

---
+++ fast_backend/backend/backend/db/models/__init__.py
@@ -0,0 +1,14 @@
+"""backend models."""
+
+from typing import Sequence, Type
+
+from beanie import Document
+
+from backend.db.models.dummy_model import DummyModel
+
+
+def load_all_models() -> Sequence[Type[Document]]:
+    """Load all models from this folder."""
+    return [
+        DummyModel,
+    ]
