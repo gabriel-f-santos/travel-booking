619a0b0b4ccc42867545d2bc9f1d615718f88269
Gabriel <gabrielfgsantos95@gmail.com>
2024-07-28 21:44:10 -0300

    feat: improving navbar

--- next-app/travel-app/src/components/navbar/NavBar.jsx
+++ next-app/travel-app/src/components/navbar/NavBar.jsx
@@ -1,26 +1,31 @@
-"use client";
-import React from 'react'
+'use client';
+import React from 'react';
 
 export default function NavBar() {
   const [showModal, setShowModal] = React.useState(false);
-  const [isScrolling, setIsScrolling] = React.useState(false);
+  const [isScrolled, setisScrolled] = React.useState(false);
 
   const toggleModal = () => {
     setShowModal(!showModal);
-  }
-  
+  };
+
   React.useEffect(() => {
     window.onscroll = () => {
-      setIsScrolling(window.pageYOffset === 0 ? false : true);
-    }
+      setisScrolled(window.pageYOffset === 0 ? false : true);
+    };
 
     return () => {
       window.onscroll = null;
-    }
-
+    };
   }, []);
 
   return (
-    <div>NavBar</div>
-  )
+    <div
+      className={`fixed z-20 h-16 w-full top-0 left-0 ${
+        isScrolled ? 'shadow-md backdrop-blur' : ''
+      }`}
+    >
+      <h1>Nav bar</h1>
+    </div>
+  );
 }
